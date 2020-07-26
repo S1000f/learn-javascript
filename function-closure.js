9171067458ed8c05103032f0dfc7897cd18f6826
dohpkim <dohp.kim@gmail.com>
2020-07-26 13:14:34 +0900

    closure

--- function-closure.js
+++ function-closure.js
@@ -27,3 +27,20 @@ var test_1 = test2("Web");
 var test_2 = test2("JavaScript");
 test_1();
 test_2();
+
+// why outputs are 3 3 3?
+for (var i = 0; i < 3; i++) {
+    setTimeout(() => console.log(i), 0);
+}
+
+// solve1: ES6 let Keyword
+for (let i = 0; i < 3; i++) {
+    setTimeout(() => console.log(i), 0);
+}
+
+// solve2: apply Closure
+for (var i = 0; i < 3; i++) {
+    (function (closed_i) {
+        setTimeout(() => console.log(closed_i), 0);
+    })(i);
+}
