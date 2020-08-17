87d02727ea35551efb94cc28866b8f1f5010b972
dohpkim <dohp.kim@gmail.com>
2020-08-17 21:24:36 +0900

    nodejs; route and response data type

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,20 +1,45 @@
-
+// module
 var express = require('express');
 
+// initiate server
 var app = express();
+app.use(express.static('public'));
 
-app.listen(54242, function () {
-    console.log('Server running at http://localhost:54242');
+// routing
+app.all('/a', (request, response) => {
+    response.send('<h1>page A</h1>');
+});
+
+// response data type
+app.all('/data.json', (req, res) => {
+    res.send(items);
+});
+app.all('/data.xml', (req, res) => {
+    var output = '';
+    output += '<?xml version="1.0" encoding="UTF-8" ?>';
+    output += '<products>';
+    items.forEach(item => {
+        output += '<product>';
+        output += '  <name>' + item.name + '</name>';
+        output += '  <price>' + item.price + '</price>';
+        output += '</product>';
+    });
+    output += '</products>';
+    res.type('text/xml');
+    res.send(output);
 })
 
-app.use(function (request, response, next) {
-    console.log('first');
-    next();
-})
-app.use(function (req, res, next) {
-    console.log('second');
-    next();
-})
+var items = [
+    {
+        name: 'milk',
+        price: '4000'
+    },
+    {
+        name: 'coffee',
+        price: '3000'
+    }
+];
+
 app.use(function (req, res, next) {
     req.test = 'request.test~';
     res.test = 'response.test!';
@@ -22,4 +47,9 @@ app.use(function (req, res, next) {
 })
 app.use(function (request, response, next) {
     response.send('<h1>' + request.test + ' : ' + response.test + '</h1>');
+})
+
+// start the server
+app.listen(54242, function () {
+    console.log('Server running at http://localhost:54242');
 })
